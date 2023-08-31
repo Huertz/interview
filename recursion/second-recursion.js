3c4cbf3737697f8e26d681cd4bd9a35240f62340
Huertz <104065687+Huertz@users.noreply.github.com>
2023-08-30 22:16:28 -0500

    second recursion

---
+++ recursion/second-recursion.js
@@ -0,0 +1,19 @@
+//! Second recursion function
+
+function sumRange(num) {
+  if (num === 1) return 1;
+  return num + sumRange(num - 1);
+}
+
+sumRange(3);
+
+//? return 3 + sumRange(2)
+//? return 2 + sumRange(1)
+//? return 1
+//? return 6
+
+//? return 4 + sumRange(3)
+//? return 3 + sumRange(2)
+//? return 2 + sumRange(1)
+//? return 1
+//? return 10
