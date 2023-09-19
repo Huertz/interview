bd18d5345a944f6c6ef2dc577aa593616a31307f
Huertz <104065687+Huertz@users.noreply.github.com>
2023-09-19 14:17:15 -0500

    added notes to naive search

--- searching/naive/naive-search.js
+++ searching/naive/naive-search.js
@@ -2,9 +2,10 @@ function naiveSearch(long, short) {
   var count = 0;
   for (var i = 0; i < long.length; i++) {
     for (var j = 0; j < short.length; j++) {
-      //! does not match break
+      //! short j equals to omg
+      //! [i + j] helps track the string
       if (short[j] !== long[i + j]) break;
-      //! matches add a to count
+      //! if index of j = 2 the omg matches it increments a score
       if (j === short.length - 1) count++;
     }
   }
