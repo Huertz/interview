5e29bc007e38ecd0b71bc55168713cf36f20b28a
Huertz <104065687+Huertz@users.noreply.github.com>
2023-10-09 17:11:24 -0500

    lienaer search

--- searching/linear/linear-search.js
+++ searching/linear/linear-search.js
@@ -1,8 +1,8 @@
 function linearSearch(arr, val) {
-  for (let i = 0; i <= arr.lenght; i++) {
-    if (arr[i] === val) {
-      return i;
-    }
+  for (let i = 0; i <= arr.length; i++) {
+    if (arr[i] === val) return i;
   }
-  return -1;
+  return false;
 }
+
+console.log(linearSearch([34, 56, 1, 2], 34));
--- searching/linear/linear-search.md
+++ searching/linear/linear-search.md
@@ -8,3 +8,10 @@
 - includes
 - find
 - findIndex
+
+# Pseidocode
+
+- This function accepts an array and a value
+- Loop through the array and check if the current array element is equal to the value
+- If it is, returned the index at which the element is found
+- If the value is never found, return -1
