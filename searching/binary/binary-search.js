8ab71b19053ab81fdd7cb125e084b77b1bc6625a
Huertz <104065687+Huertz@users.noreply.github.com>
2023-10-09 18:02:32 -0500

    binary search

--- searching/binary/binary-search.js
+++ searching/binary/binary-search.js
@@ -1,16 +1,18 @@
-function binarySearch(arr, ele) {
+function binarySearch(arr, e) {
   //! implements start, middle, end
   let start = 0;
   let end = arr.length - 1;
   var middle = Math.floor((start + end) / 2);
-  while (arr[middle] !== ele && start <= end) {
+  console.log(start, middle, end);
+
+  while (arr[middle] !== e && start <= end) {
     //! other way to code if else statements
-    if (ele < arr[middle]) end = middle - 1;
+    if (e < arr[middle]) end = middle - 1;
     else start = middle + 1;
     middle = Math.floor((start + end) / 2);
     console.log(start, middle, end);
   }
-  return arr[middle] === ele ? middle : -1;
+  return arr[middle] === e ? middle : -1;
 }
 
-console.log(binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 13));
+console.log(binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 40));
--- searching/binary/binary-search.md
+++ searching/binary/binary-search.md
@@ -5,3 +5,15 @@
 - Binary search only workds on sorted arrays
 
 # Divide and Conquer
+
+- Pick up a point in the middle of the array basically
+
+# Psedocode
+
+- This function accepts a sorted array and a value
+- Create a left pointer at the start of the array, and a right pointer at the end of the array
+- While the left pointer comes before the right pointer
+  - Create a pointer in the middle
+  - If you find the value you want, return the index
+  - If the value is too small, move the left pointer up
+- If you never find the value, return -1
