8db7024ea122142e0ed5bf6e07da451bfe3a58df
Huertz <104065687+Huertz@users.noreply.github.com>
2023-10-09 18:21:31 -0500

    binary search version

--- searching/binary/binary-search.js
+++ searching/binary/binary-search.js
@@ -1,17 +1,38 @@
+// Origianl Solution
+// function binarySearch1(arr, e) {
+//  implements start, middle, end
+//   let start = 0;
+//   let end = arr.length - 1;
+//   var middle = Math.floor((start + end) / 2);
+
+//   while (arr[middle] !== e && start <= end) {
+//     if (e < arr[middle]) {
+//       end = middle - 1;
+//     } else {
+//       start = middle + 1;
+//     }
+//     middle = Math.floor((start + end) / 2);
+//   }
+//   if (arr[middle] === e) {
+//     return middle;
+//   }
+//   return -1;
+// }
+//
+
+// Refactor Version
 function binarySearch(arr, e) {
   //! implements start, middle, end
   let start = 0;
   let end = arr.length - 1;
   var middle = Math.floor((start + end) / 2);
-  console.log(start, middle, end);
 
   while (arr[middle] !== e && start <= end) {
-    //! other way to code if else statements
     if (e < arr[middle]) end = middle - 1;
     else start = middle + 1;
     middle = Math.floor((start + end) / 2);
-    console.log(start, middle, end);
   }
+
   return arr[middle] === e ? middle : -1;
 }
 
