47fb0e682f433bfc83a79d108a1970139cb7df5d
Huertz <104065687+Huertz@users.noreply.github.com>
2023-10-11 00:21:52 -0500

    merging arrays

---
+++ sorting/merge/merge-sort.js
@@ -0,0 +1,26 @@
+// merge function
+function merge(arr1, arr2) {
+  let results = [];
+  // arr1
+  let i = 0;
+  // arr2
+  let j = 0;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr2[j] > arr1[i]) {
+      results.push(arr1[i]);
+      i++;
+    } else {
+      results.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    results.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    results.push(arr2[j]);
+    j++;
+  }
+  return results;
+}
